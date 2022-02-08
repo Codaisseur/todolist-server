4b56adb1c455b699040278834017a2e567dfcdd7
Matias Garcia <matiasigarcia91@gmail.com>
2022-02-08 11:27:02 +0100

    fix bug in routes

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = 4000;
 const app = express();
 
-const userRouter = require('./routers/user');
-const todoListRouter = require('./routers/todoList');
-
-const ourMiddleWare = (req, res, next) => {
-  console.log('We happened in the middle');
-  next();
-};
+const userRouter = require("./routers/user");
+const todoListRouter = require("./routers/todoList");
 
 const randomAuthMiddleware = (req, res, next) => {
   const randomNumber = Math.ceil(Math.random() * 10);
-  console.log('The random number', randomNumber);
+  console.log("The random number", randomNumber);
 
   if (randomNumber <= 5) next();
-  if (randomNumber > 5 && randomNumber <= 10) res.status(401).send('FAILED!!');
+  if (randomNumber > 5 && randomNumber <= 10) res.status(401).send("FAILED!!");
 };
 
-const anotherMiddleware = (req, res, next) => {
+const secondMiddleware = (req, res, next) => {
   const randomNumber = Math.ceil(Math.random() * 10);
-  console.log('The random number', randomNumber);
+  console.log("The random number", randomNumber);
 
   if (randomNumber <= 3) next();
   if (randomNumber > 3 && randomNumber <= 10)
-    res.status(401).send('Not even my final form!!');
+    res.status(401).send("Not even my final form!!");
 };
+app.use(ourMiddleWare);
 
 app.use(express.json()); // parses the body
 
-app.use(ourMiddleWare);
-
 app.get(
-  '/',
+  "/",
   randomAuthMiddleware,
   anotherMiddleware,
   async (req, res, next) => {
@@ -46,11 +40,11 @@ app.get(
   }
 );
 
-app.get('/test', randomAuthMiddleware, (req, res) => {
-  res.send('testing! :)');
+app.get("/test", randomAuthMiddleware, (req, res) => {
+  res.send("testing! :)");
 });
 
-app.use('/users', userRouter);
+app.use("/users", userRouter);
 app.use(todoListRouter);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- routers/todoList.js
+++ routers/todoList.js
@@ -1,25 +1,29 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = new Router();
 
-const User = require('../models').user;
-const TodoList = require('../models').todoList;
+const User = require("../models").user;
+const TodoList = require("../models").todoList;
 
-router.post('/lists/:userId', async (req, res, next) => {
+router.post("/lists/:userId", async (req, res, next) => {
   try {
     const { name } = req.body;
     const { userId } = req.params;
 
     const userExists = await User.findByPk(parseInt(userId));
 
-    if (!userExists) return res.status(404).send({ message: 'User not found' });
+    if (!userExists) {
+      return res.status(404).send({ message: "User not found" });
+    }
 
-    if (!name)
-      return res.status(400).send({ message: 'Please provide a name' });
-
+    if (!name) {
+      return res.status(400).send({ message: "Please provide a name" });
+    }
     const newList = await TodoList.create({
       name: name,
       ownerId: parseInt(userId),
     });
+
+    console.log("new list created");
     res.status(200).send(newList);
   } catch (error) {
     console.log(error);
